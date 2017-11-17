6d2580279902b42320cdeed79ebbb4581e34df0c
Unknown <a.canutperdigon@gmail.com>
2017-11-17 18:20:46 +0100

    El cross va bien, y añadidas mas propiedades a la clase vector3D

--- Assets/Vector3D.cs
+++ Assets/Vector3D.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Vector3D {
-    public float x, y, z;
-
+    public float x, y, z, w;
+    public Vector3D front = new Vector3D(0, 0, 1);
+    public Vector3D up = new Vector3D(0, 1, 0);
+    public Vector3D right = new Vector3D(1, 0, 0);
     public Vector3D() {
-        x = y = z = 0;
+        x = y = z = w = 0;
     }
     public Vector3D(float n) {
         x = y = z = n;
+        w = 0;
     }
     public Vector3D(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
+        w = 0;
     }
 
     public Vector3D Normalized() {
@@ -33,10 +37,26 @@ public class Vector3D {
 
     public static Vector3D Cross(Vector3D v1, Vector3D v2) {
         Vector3D cross = new Vector3D();
-        cross.x = v1.y * v2.z - v1.z * v2.y;
-        cross.y = v1.z * v2.x - v1.x * v2.z;
-        cross.z = v1.x * v2.y - v1.y * v2.x;
+        cross.x = v1.y * v2.z - v2.y * v1.z;
+        cross.y = -(v1.x * v2.z - v2.x * v1.z);
+        cross.z = v1.x * v2.y - v2.x * v1.y;
         
         return cross;
+    }
+
+    public static Vector3D operator + (Vector3D c1, Vector3D c2) {
+        return new Vector3D(c1.x + c2.x, c1.y + c2.y, c1.z + c2.z);
+    }
+
+    public static Vector3D operator -(Vector3D c1, Vector3D c2) {
+        return new Vector3D(c1.x - c2.x, c1.y - c2.y, c1.z - c2.z);
+    }
+
+    public static Vector3D operator *(Vector3D c1, Quaternion c2) {
+        return new Vector3D();
+    }
+    public static Vector3D operator *(Quaternion q, Vector3D v) {
+
+        return new Vector3D();
     }
 }
--- Assets/stuff.cs
+++ Assets/stuff.cs
@@ -7,6 +7,9 @@ public class stuff : MonoBehaviour {
 	void Update () {
         Debug.Log(Vector3.Cross(new Vector3(10, 4, 3), new Vector3(-1, 0, 0)));
         Vector3D mycross = Vector3D.Cross(new Vector3D(10, 4, 3), new Vector3D(-1, 0, 0));
-        Debug.Log("("+mycross.x+","+mycross.y+","+mycross.y+")");
+        Debug.Log("(" + mycross.x + "," + mycross.y + "," + mycross.z + ")");
+
+        //float dot = Vector3D.Dot(new Vector3D(10, 4, 3), new Vector3D(-1, 0, 0));
+        //Debug.Log(dot);
     }
 }
