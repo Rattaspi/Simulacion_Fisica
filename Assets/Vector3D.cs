e547f6df11cedf87a24aa2ed2eb04c2e88003935
Unknown <a.canutperdigon@gmail.com>
2017-11-17 13:45:20 +0100

    Hay una clase que es Vector3D, tiene 3 sobrecargas de constructor, el metodo Normalize, Dot y Cross. Hay un problemilla en el Cross que tengo que arreglar.

---
+++ Assets/Vector3D.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3D {
+    public float x, y, z;
+
+    public Vector3D() {
+        x = y = z = 0;
+    }
+    public Vector3D(float n) {
+        x = y = z = n;
+    }
+    public Vector3D(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3D Normalized() {
+        Vector3D vec = new Vector3D();
+        float mag = Mathf.Sqrt(x * x + y * y + z * z);
+        vec.x = x / mag;
+        vec.y = y / mag;
+        vec.z = z / mag;
+        return vec;
+    }
+
+    public static float Dot(Vector3D v1, Vector3D v2) {
+        float dotProduct = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        return dotProduct;
+    }
+
+    public static Vector3D Cross(Vector3D v1, Vector3D v2) {
+        Vector3D cross = new Vector3D();
+        cross.x = v1.y * v2.z - v1.z * v2.y;
+        cross.y = v1.z * v2.x - v1.x * v2.z;
+        cross.z = v1.x * v2.y - v1.y * v2.x;
+        
+        return cross;
+    }
+}
---
+++ Assets/stuff.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stuff : MonoBehaviour {	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Vector3.Cross(new Vector3(10, 4, 3), new Vector3(-1, 0, 0)));
+        Vector3D mycross = Vector3D.Cross(new Vector3D(10, 4, 3), new Vector3D(-1, 0, 0));
+        Debug.Log("("+mycross.x+","+mycross.y+","+mycross.y+")");
+    }
+}
