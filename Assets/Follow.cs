936547edad10a7dbe3d6b5127ce9eb00c11c1d08
Unknown <a.canutperdigon@gmail.com>
2017-11-22 19:54:15 +0100

    Ya esta el bracico

---
+++ Assets/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    public GameObject endEffector;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = endEffector.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = endEffector.transform.position;
+    }
+}
--- Assets/Quat.cs
+++ Assets/Quat.cs
@@ -2,26 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyQuat {
+public class Quat {
     public float w, x, y, z;
     // Use this for initialization
 
-    public MyQuat(float w,float x, float y, float z) {
+    public Quat(float w,float x, float y, float z) {
         this.w = w;
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public MyQuat() {
+    public Quat() {
         w = 0;
         x = 0;
         y = 0;
         z = 0;
     }
 
-    public static MyQuat Normalize(MyQuat quat) {
-        MyQuat temp = new MyQuat();
+    public static Quat Normalize(Quat quat) {
+        Quat temp = new Quat();
         float modulo = Mathf.Sqrt(quat.w * quat.w + quat.x * quat.x + quat.y * quat.y + quat.z * quat.z);
         temp = quat;
         temp.w /= modulo;
@@ -31,14 +31,12 @@ public class MyQuat {
         return temp;
     }
 
-    public static MyQuat Inverse(MyQuat quat) {
-
-        return new MyQuat(quat.w, -quat.x, -quat.y, -quat.z);
+    public static Quat Inverse(Quat quat) {
+        return new Quat(quat.w, -quat.x, -quat.y, -quat.z);
     }
 
-    public static MyQuat Multiply(MyQuat quat1, MyQuat quat2) {
-
-        MyQuat temp = new MyQuat();
+    public static Quat operator *(Quat quat1, Quat quat2) {
+        Quat temp = new Quat();
 
         temp.w = quat1.w * quat2.w - quat1.x * quat2.x - quat1.y * quat2.y - quat1.z * quat2.z;
         temp.x = quat1.x * quat2.w + quat1.w * quat2.x + quat1.y * quat2.z - quat1.z * quat2.y;
@@ -47,7 +45,7 @@ public class MyQuat {
         return temp;
     }
 
-    public static void SetRotation(GameObject a, MyQuat myQuat) {
+    public static void SetRotation(GameObject a, Quat myQuat) {
         Quaternion result;
         result.w = myQuat.w;
         result.x = myQuat.x;
@@ -64,32 +62,30 @@ public class MyQuat {
         Debug.Log("z = " + z);
     }
 
-    public static MyQuat EulerToMyQuat(float yaw, float pitch, float roll) 
-{
-            float rollOver2 = roll * 0.5f;
-            float sinRollOver2 = Mathf.Sin(rollOver2);
-            float cosRollOver2 = Mathf.Cos(rollOver2);
-            float pitchOver2 = pitch * 0.5f;
-            float sinPitchOver2 = Mathf.Sin(pitchOver2);
-            float cosPitchOver2 = Mathf.Cos(pitchOver2);
-            float yawOver2 = yaw * 0.5f;
-            float sinYawOver2 = Mathf.Sin(yawOver2);
-            float cosYawOver2 = Mathf.Cos(yawOver2);
-            MyQuat result = new MyQuat();
-            result.x = cosYawOver2 * cosPitchOver2 * cosRollOver2 + sinYawOver2 * sinPitchOver2 * sinRollOver2;
-            result.y = cosYawOver2 * cosPitchOver2 * sinRollOver2 - sinYawOver2 * sinPitchOver2 * cosRollOver2;
-            result.z = cosYawOver2 * sinPitchOver2 * cosRollOver2 + sinYawOver2 * cosPitchOver2 * sinRollOver2;
-            result.w = sinYawOver2 * cosPitchOver2 * cosRollOver2 - cosYawOver2 * sinPitchOver2 * sinRollOver2;
-            return result;
-        }
+    public static Quat EulerToMyQuat(float yaw, float pitch, float roll){
+        float rollOver2 = roll * 0.5f;
+        float sinRollOver2 = Mathf.Sin(rollOver2);
+        float cosRollOver2 = Mathf.Cos(rollOver2);
+        float pitchOver2 = pitch * 0.5f;
+        float sinPitchOver2 = Mathf.Sin(pitchOver2);
+        float cosPitchOver2 = Mathf.Cos(pitchOver2);
+        float yawOver2 = yaw * 0.5f;
+        float sinYawOver2 = Mathf.Sin(yawOver2);
+        float cosYawOver2 = Mathf.Cos(yawOver2);
+        Quat result = new Quat();
+        result.x = cosYawOver2 * cosPitchOver2 * cosRollOver2 + sinYawOver2 * sinPitchOver2 * sinRollOver2;
+        result.y = cosYawOver2 * cosPitchOver2 * sinRollOver2 - sinYawOver2 * sinPitchOver2 * cosRollOver2;
+        result.z = cosYawOver2 * sinPitchOver2 * cosRollOver2 + sinYawOver2 * cosPitchOver2 * sinRollOver2;
+        result.w = sinYawOver2 * cosPitchOver2 * cosRollOver2 - cosYawOver2 * sinPitchOver2 * sinRollOver2;
+        return result;
+    }
 
 
-    public static MyQuat AxisAngleToMyQuat(Vector3 axis, float angle) {
-
+    public static Quat AxisAngleToMyQuat(Vector3 axis, float angle) {
         float localAngle = angle * Mathf.Deg2Rad;
 
         Vector3 temp = axis.normalized;
-        MyQuat result = new MyQuat();
+        Quat result = new Quat();
         result.w = Mathf.Cos(localAngle / 2);
         result.x = temp.x * Mathf.Sin(localAngle / 2);
         result.y = temp.y * Mathf.Sin(localAngle / 2);
@@ -99,11 +95,8 @@ public class MyQuat {
         return result;
     }
 
-    public static void MyQuatToAxisAngle(MyQuat myQuat,Vector3 axis, float angle) {
-
-        MyQuat temp = temp = Normalize(myQuat);
-
-
+    public static void MyQuatToAxisAngle(Quat myQuat,Vector3 axis, float angle) {
+        Quat temp = temp = Normalize(myQuat);
 
         angle = Mathf.Rad2Deg * 2 * Mathf.Acos(myQuat.w);
         axis.x = temp.x / Mathf.Sqrt(1 - temp.w * temp.w);
@@ -112,8 +105,7 @@ public class MyQuat {
     }
 
 
-    public static void toEulerAngle(MyQuat q, float yaw, float pitch, float roll)
-{
+    public static void toEulerAngle(Quat q, float yaw, float pitch, float roll){
         roll = Mathf.Atan2(q.w * q.y + q.x * q.z, q.w * q.z - q.x * q.y);
         pitch = Mathf.Acos(-q.w * q.w - q.x * q.x + q.y * q.y + q.z * q.z);
         yaw = Mathf.Atan2(q.w * q.y - q.x * q.z, q.x * q.y + q.w * q.z);
